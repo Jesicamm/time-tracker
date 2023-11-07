076712d65e7e92c7b2c8ed09e8cf6842ef119848
jesica2 <jesica@belikesoftware.com>
2023-11-07 19:49:39 +0100

    added font

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,14 @@
+import TimeTracker from "./timeTracker/TimeTracker"
 import "./header.css"
 
 const Header: React.FC = () => {
-  return <div className="header-content">I'm application header</div>
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <TimeTracker />
+      </div>
+    </div>
+  )
 }
 
 export default Header
---
+++ src/components/header/timeTracker/TimeTracker.tsx
@@ -0,0 +1,7 @@
+import "./timeTracker.css"
+
+const TimeTracker: React.FC = () => {
+  return <div className="time-tracker-container">Time Tracker</div>
+}
+
+export default TimeTracker
